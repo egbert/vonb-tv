67cca6e65f6350e8a1ee36e77264daf6859b8ebe
Egbert Veenstra <egbertveenstra@gmail.com>
2014-02-09 19:10:19 +0100

    add first content

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~> 3.2.0'
 gem 'slim'
+gem 'coffee-script'
+gem 'sass'
--- config.rb
+++ config.rb
@@ -3,3 +3,9 @@ set :layout, false
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :fonts_dir, 'fonts'
+
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
