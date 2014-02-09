ca1e02f9f9fa8d69eee50ed76a5818fa2407bc37
Egbert Veenstra <egbertveenstra@gmail.com>
2014-02-09 15:44:55 +0100

    setup middleman development configuration

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 3.2.0'
+gem 'slim'
---
+++ config.rb
@@ -0,0 +1,5 @@
+set :layout, false
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
